96c415ad29bdc6599fc48e594055c3e9408ad0d7
Bogdan Chernov <bogdan.chernov@volia.com>
2022-03-10 17:42:11 +0200

    v11

--- JenkinsPipeLine.groovy
+++ JenkinsPipeLine.groovy
@@ -32,6 +32,9 @@ pipeline {
             }
         }
         stage('Download') {
+            String GITHUB_ACCESS_TOKEN ="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"
+            String GITHUB_USERNAME = "BohdanChernov"
+            String REPOSITORY_NAME = "alarm"
              steps {
                  echo 'Hello World Download'
 
@@ -43,7 +46,11 @@ pipeline {
 
                  sh 'git add files/.'
                  sh 'git commit -m "added files"'
-                 sh 'git push origin master'
+
+//                 sh 'git push https://<GITHUB_ACCESS_TOKEN>@github.com/<GITHUB_USERNAME>/<REPOSITORY_NAME>.git'
+                 sh 'git push https://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@github.com/BohdanChernov/alarm.git HEAD:master'
+
+//                 sh 'git push origin master'
                  echo 'Hello World Download Finish'
              }
         }
